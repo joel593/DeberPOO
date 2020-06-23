fb1088ba061b59c1ba75999e9419b1803bad03d1
joel593 <drouet593@gmail.com>
2020-06-23 19:52:32 +0200

    Deber

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AplicacionConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            if (args.Length > 0)
+            {
+                Console.Write(args[0]);
+            }
+
+            Console.Write("Hola, bienvenido al centro de experimentacion magna ");
+            Console.WriteLine("¿Cúal es tu nombre?");
+            string datosIngresados = Console.ReadLine();
+            Console.WriteLine("¿Cuantos años tenes?");
+            int datosIngresados3 = Console.Read();
+            Console.WriteLine("¿interesante quieres saber lo que te haremos? " + datosIngresados);
+            string datosIngresados2 = Console.ReadLine();
+
+        }
+     
+    }
+    }
+
